de52f6ffe7f8e811006f1a859e176241edacc4d0
Abdul-Ahad <121967743+ahad324@users.noreply.github.com>
2023-02-17 23:30:34 +0500

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,19 @@
+
+//  const ico=document.querySelector('.detail summary i')
+
+
+//  const det=document.querySelector('.detail').addEventListener('clcik',()=>{
+//     ico.classList.add('fa-solid fa-angle-up')
+// })
+$('.menu-btn').click(function () {
+    $('nav .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+    $('.menu-btn i').toggleClass("fa-sharp fa-solid fa-xmark");
+    $('nav').toggleClass("active");
+})
+$('.deta1').click(function () {
+    $('.deta1 summary i').toggleClass("fa-angle-up");
+})
+$('.deta2').click(function () {
+    $('.deta2 summary i').toggleClass("fa-angle-up");
+})
